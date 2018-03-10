109a7a24e5e0c8c31f4388b4994e866af2d5c297
Phillip Hartin <mail@philliphartin.com>
2018-03-10 14:54:53 +1100

    Adding Prettier, SCSS and Pug to project along with basic template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    .router-container
+      router-view
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App"
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "styles/variables/_colors.scss";
+
+html {
+  display: block;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background: white;
+  display: block;
+  height: 100%;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: 100%;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  background: $color2;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.router-container {
+  position: relative;
+  margin: 2rem;
 }
 </style>
