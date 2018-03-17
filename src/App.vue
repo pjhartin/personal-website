fa618e7019d280233ee4c84a6f4a19cbe9e991f4
Phillip Hartin <mail@philliphartin.com>
2018-03-17 17:47:26 +1100

    Updating welcome/landing page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template lang="pug">
   #app
-    .router-container
-      router-view
+    .top-nav
+      .links
+        a.external-link(href="mailto:contact@philliphartin.com", target="_blank")
+          i.fal.fa-envelope
+        a.external-link(href="https://medium.com/@philliphartin", target="_blank") 
+          i.fab.fa-medium-m
+        a.external-link(href="https://github.com/pjhartin", target="_blank")
+          i.fab.fa-github
+        a.external-link(href="https://twitter.com/philliphartin", target="_blank")
+          i.fab.fa-twitter
+        a.external-link(href="https://www.linkedin.com/in/philliphartin/", target="_blank")
+          i.fab.fa-linkedin-in
+    .content
+      .router-container
+        router-view
 </template>
 
 <script>
@@ -21,7 +34,7 @@ html {
 body {
   margin: 0;
   padding: 0;
-  background: white;
+  background: $purple;
   display: block;
   height: 100%;
 }
@@ -29,16 +42,46 @@ body {
 #app {
   height: 100%;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  background: $color4;
+  background: $purple;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   display: flex;
+  flex-direction: column;
+}
+
+.top-nav {
+  position: relative;
+  height: 80px;
+  display: flex;
+}
+
+.content {
+  display: flex;
+  flex: 1;
   align-items: center;
   justify-content: center;
 }
-
 .router-container {
   position: relative;
-  margin: 12rem;
+  width: 100%;
+}
+
+.links {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin: 0 2rem;
+
+  .external-link {
+    color: white;
+    transition: all 350ms ease-in-out;
+    padding: 0.5rem;
+    font-size: 1rem;
+
+    &:hover {
+      color: $orange;
+    }
+  }
 }
 </style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,15 +1,23 @@
 <template lang="pug">
   
   .welcome
-    h1 Phillip Hartin
-    p I've been researching and building stuff, for quite some time now. I've been pretty busy with all the work. So busy in fact, that I haven't had a chance to build my own site. Good news is, I'm working on it.
-  
-    .links
-      a.external-link(href="mailto:contact@philliphartin.com", target="_blank") Email.
-      a.external-link(href="https://github.com/pjhartin", target="_blank") Github.
-      a.external-link(href="https://medium.com/@philliphartin", target="_blank") Medium.
-      a.external-link(href="https://twitter.com/philliphartin", target="_blank") Twitter.
-      a.external-link(href="https://www.linkedin.com/in/philliphartin/", target="_blank") LinkedIn.
+
+    .content
+      
+      .title
+        .letters
+          .row
+            .letter H
+            .letter.offset A
+            .letter R
+          .row
+            .letter T
+            .letter I
+          .row
+            .letter N
+
+        .tagline Building tech for the web
+        //-p I've been researching and building stuff, for quite some time now. I've been pretty busy with all the work. So busy in fact, that I haven't had a chance to build my own site. Good news is, I'm working on it.
 
 </template>
 
@@ -25,23 +33,64 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../styles/variables/_colors.scss";
+@import "../styles/variables/_fonts.scss";
 
 .welcome {
   color: white;
-  font-family: monospace;
 
-  .links {
-    > .external-link {
-      margin-right: 1rem;
-      color: white;
-      transition: all 350ms ease-in-out;
-      // mix-blend-mode: difference;
-      // text-decoration: none;
+  .content {
+    // padding: 0 12rem;
+  }
 
-      &:hover {
-        color: $color1;
+  .title {
+    position: relative;
+    text-align: center;
+    max-width: 600px;
+    width: 100%;
+  }
+
+  .letters {
+    // font-family: $font-cabin;
+    font-family: "Work Sans";
+
+    font-size: 9rem;
+    color: black;
+    font-weight: 700;
+
+    .row {
+      margin: -0.1em 0rem;
+      display: flex;
+      align-content: center;
+      align-items: center;
+
+      &:nth-child(2) {
+        margin: 0 15%;
+      }
+
+      &:nth-child(3) {
+        margin: -10% 0%;
       }
     }
+
+    .letter {
+      flex: 1;
+
+      &.offset {
+        transform: translateY(10%);
+      }
+    }
+  }
+
+  .tagline {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family: $font-roadrage;
+    width: 100%;
+    font-size: 4rem;
+    line-height: 1.3em;
+    text-align: center;
   }
 }
 </style>
