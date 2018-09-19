7c3bd5cafd39452e72f287d04a8a4875eb1f1bf1
Phillip Hartin <mail@philliphartin.com>
2018-09-19 21:50:11 +1000

    Updates style and content of work page

--- pages/work.vue
+++ pages/work.vue
@@ -1,9 +1,11 @@
 <template>
-  <section class="h-100 w-100 flex  content-center justify-center font-serif flex-col text-center">
-    <div class="container mx-auto pl-8 pr-8  mt-24 mb-12">
-      <div class="work mb-12" v-for="(project, index) in projects" :key="index">
-        <a class="title text-3xl md:text-6xl no-underline text-black" :href="project.link" target="_blank">{{project.title}}<span class="year text-2xl ml-4">{{project.year}}</span></a>
-        <div class="reveal text-xl text-grey-dark mt-2">{{project.role}}</div>
+  <section class="h-100 w-100 flex content-center justify-center font-serif flex-col text-center">
+    <div class="container mx-auto pl-8 pr-8 mt-24 mb-12">
+      <div class="work mb-24" v-for="(project, index) in projects" :key="index">
+        <a class="title text-3xl md:text-5xl no-underline text-black" :href="project.link" target="_blank">{{project.title}}<span class="year text-2xl ml-4">{{project.year}}</span></a>
+        <div v-if="project.role" class="role text-2xl text-grey-dark mt-4">{{project.role}}</div>
+      <div v-if="project.subtitle" class="subtitle text text-grey-dark mt-4">{{project.subtitle}}</div>    
+        <div v-if="project.tech" class="tech text-sm text-grey-darker mt-4">{{project.tech}}</div>
       </div>
     </div>
   </section>
@@ -17,49 +19,53 @@ export default {
         {
           title: 'Healthy Taps',
           year: '2018',
-          role: 'Developer / Architect'
+          role: 'Developer',
+          tech: 'Ionic, Firebase, Vue'
         },
         {
           title: 'The Design Conference',
           year: '2017',
           role: 'Developer',
-          link: 'https://thedesignconference.com.au'
+          link: 'https://thedesignconference.com.au',
+          tech: 'Wordpress, Braintree'
         },
         {
           title: 'Analogue Gallery',
           year: '2016',
           role: 'Developer',
-          link: 'https://analoguegallery.com.au'
+          link: 'https://analoguegallery.com.au',
+          tech: 'VanillaJS'
         },
         {
           title: 'First Coat Festival App',
           year: '2016',
           role: 'Developer',
-          link: 'https://firstcoat.com.au/'
+          link: 'https://firstcoat.com.au/',
+          tech: 'Ionic, Angular, Silverstripe'
         },
         {
           title: 'The Design Conference App',
+          subtitle: 'AGDA Design Awards 2016 - Winner - Digital Design',
           year: '2016',
           role: 'Developer / UX Designer',
-          link: 'http://awards2016.agda.com.au/finalist/3/10005/624'
+          link: 'http://awards2016.agda.com.au/finalist/3/10005/624',
+          tech: 'Ionic, Angular, Silverstripe'
         },
         {
           title: 'Gray Matters',
-          year: '2015',
-          role: 'Developer / UI & UX Designer',
-          link: 'http://graymattersapp.org'
-        },
-        {
-          title: 'Gray Matters (RCT)',
-          year: '2014',
+          subtitle: 'USA / Italy RCT study & original public MVP',
+          year: '2014-2016',
           role: 'Developer / Data Scientist',
-          link: 'http://graymattersapp.org'
+          link: 'http://graymattersapp.org',
+          tech: 'iOS, Android, Ionic, NodeJS, MongoDb, PHP, Matlab, SPSS'
         },
         {
           title: 'TAUT App',
           year: '2013',
           role: 'Developer / Data Scientist',
-          link: 'https://link.springer.com/chapter/10.1007/978-3-319-13105-4_53'
+          link:
+            'https://link.springer.com/chapter/10.1007/978-3-319-13105-4_53',
+          tech: 'Android, PHP, Matlab, R'
         }
       ]
     };
