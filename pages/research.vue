78a0e4cffb1545201105ea4b1accbc832137979a
Phillip Hartin <mail@philliphartin.com>
2018-09-20 10:18:39 +1000

    Updates to responsive text sizes

--- pages/research.vue
+++ pages/research.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="h-full w-full flex content-center justify-center font-serif flex-col text-center">
-    <div class="container mx-auto md:w-1/2 pl-8 pr-8 mt-24 mb-12">
+    <div class="container mx-auto md:w-2/3 lg:w-1/2 pl-8 pr-8 mt-24 mb-12">
       <div class="work list-item mb-24 relative" v-for="(publication, index) in publications" :key="index">
 
         <a :href="publication.link" target="_blank" class="no-underline text-black">
-          <div class="title text-3xl md:text-3xl no-underline text-black" >{{publication.title}}</div>
-          <div v-if="publication.authors" class="text-xl text-grey-darker mt-4">{{publication.authors}}</div>
+          <div class="title text-2xl md:text-3xl no-underline text-black" >{{publication.title}}</div>
+          <div v-if="publication.authors" class="text-lg md:text-xl text-grey-darker mt-4">{{publication.authors}}</div>
           <div v-if="publication.publisher" class="text text-grey-dark mt-4">{{publication.publisher}}</div>
           <div v-if="publication.year" class="text-xl text-grey-dark mt-4">{{publication.year}}</div>
 
