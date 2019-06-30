f603e9a65f5f2f52363e54b67624446ba7b9d767
pjhartin <mail@philliphartin.com>
2019-07-01 09:56:37 +1000

    Adds content to about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,8 +3,25 @@
     class="blend-difference min-h-screen w-full flex content-center justify-center font-serif flex-col text-center text-white"
   >
     <div class="container mx-auto md:w-2/3 pl-8 pr-8 mt-24 mb-12">
-      <p class="text-xl mb-2">Hi, I'm Phillip Hartin</p>
-      <p class="text-xl">I've been working...</p>
+      <p class="md:text-2xl mb-4">
+        Hi, I'm Phillip Hartin,
+        <br>thanks for visiting my little slice of the internet.
+      </p>
+      <p class="md:text-2xl mb-4"></p>
+      <p class="md:text-2xl mb-4">
+        I'm currently the
+        <b>Engineering & Development Lead at Big Picture Medical</b>, a medical technology company working to set the global standard in the delivery of specialist quality care.
+      </p>
+      <p
+        class="md:text-2xl mb-4"
+      >Outside of this role, I'm involved in a number of healthcare research projects on a software consultancy basis, developing mobile apps, web platforms, and all their supporting infrastructures.</p>
+      <!-- <p class="md:text-2xl mb-4">
+        Outside of this role, I'm involved in a range of healthcare research projects and creative projects as the co-founder of
+        <b>LittleFox Studios</b>.
+      </p>-->
+      <p
+        class="md:text-2xl mb-4"
+      >These commitments keep me very busy, so I apologise if I'm slow to respond, but if you'd like to get in touch, please reach out to me on one of the following:</p>
     </div>
   </section>
 </template>
